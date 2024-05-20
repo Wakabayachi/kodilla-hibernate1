3f1ccf62d06a0b7dfa6e1da7e6700b3886e7c519
Daniel Nastalski <maspa1987@gmail.com>
2024-05-20 22:40:28 +0200

    first commit

---
+++ src/main/java/com/kodilla/kodillahibernate1/KodillaHibernate1Application.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillahibernate1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaHibernate1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHibernate1Application.class, args);
+    }
+
+}
